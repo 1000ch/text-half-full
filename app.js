d8a4d4d6ed284796c36f1539757f8682d8bb67a0
1000ch <shogo.sensui@gmail.com>
2016-01-31 02:25:26 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function toFullWidth(string) {
+  return string.replace(/[A-Za-z0-9\-]/g, s => String.fromCharCode(s.charCodeAt(0) + 0xFEE0));
+}
+
+function toHalfWidth(string) {
+  return string.replace(/[Ａ-Ｚａ-ｚ０-９－]/g, s => String.fromCharCode(s.charCodeAt(0) - 0xFEE0));
+}
+
+window.onload = e => {
+  const fullInput  = document.querySelector('#full-input');
+  const fullOutput = document.querySelector('#full-output');
+  const halfInput  = document.querySelector('#half-input');
+  const halfOutput = document.querySelector('#half-output');
+
+  fullInput.addEventListener('input', e => fullOutput.value = toFullWidth(fullInput.value));
+  halfInput.addEventListener('input', e => halfOutput.value = toHalfWidth(halfInput.value));
+};
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('service-worker.js', {
+    scope: '/text-half-full/'
+  }).catch(error => console.error(error));
+}
